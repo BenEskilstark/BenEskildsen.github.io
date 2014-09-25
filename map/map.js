74934ff24a2164141dfbcb76e7f2f2feb977e2c3
Benjamin Eskildsen <benjamin.eskildsen@yale.edu>
2014-09-25 13:32:22 -0400

    Fixing up map.css

--- map/map.js
+++ map/map.js
@@ -1,10 +1,13 @@
 var WORLDJSON = "world-50m.json";
+d3.json(WORLDJSON, function(error, world){
+  console.log(world);
+})
 
 var width = 960;
 var height = 700;
 
 var projection = d3.geo.equirectangular()
-    .scale(100)
+    .scale(200)
     .translate([width / 2, height / 2]);
 
 var svg = d3.select("body").append("svg")
@@ -17,5 +20,11 @@ d3.json(WORLDJSON, function(error, world) {
   svg.append("path")
     .datum(topojson.feature(world, world.objects.countries))
     .attr("d", d3.geo.path().projection(projection));
+
+    svg.selectAll(".country")
+      .data(topojson.feature(world, world.objects.countries).features)
+    .enter().append("path")
+      .attr("class", function(d) { return "country_" + d.id; })
+      .attr("d", path);
 
 });
