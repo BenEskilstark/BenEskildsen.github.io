cc832671eba406ef4aa795321a5ab7d088fb8267
Benjamin Eskildsen <benjamin.eskildsen@yale.edu>
2014-09-25 13:24:37 -0400

    Added map.css

--- map/map.js
+++ map/map.js
@@ -3,6 +3,10 @@ var WORLDJSON = "world-50m.json";
 var width = 960;
 var height = 700;
 
+var projection = d3.geo.equirectangular()
+    .scale(500)
+    .translate([width / 2, height / 2]);
+
 var svg = d3.select("body").append("svg")
   .attr("width", width)
   .attr("height", height);
@@ -12,6 +16,6 @@ d3.json(WORLDJSON, function(error, world) {
   
   svg.append("path")
     .datum(topojson.feature(world, world.objects.countries))
-    .attr("d", d3.geo.path().projection(d3.geo.equirectangular()));
+    .attr("d", d3.geo.path().projection(projection));
 
 });
