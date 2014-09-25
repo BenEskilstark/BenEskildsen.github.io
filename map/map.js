6b29b7d206afd495c296ae180e42bb7f635b4b97
Benjamin Eskildsen <benjamin.eskildsen@yale.edu>
2014-09-25 13:21:45 -0400

    Made skeleton sag for displaying world

--- map/map.js
+++ map/map.js
@@ -1,7 +1,17 @@
-console.log("map.js running");
 var WORLDJSON = "world-50m.json";
+
+var width = 960;
+var height = 700;
+
+var svg = d3.select("body").append("svg")
+  .attr("width", width)
+  .attr("height", height);
 
 d3.json(WORLDJSON, function(error, world) {
   if (error) return console.error(error);
-  console.log(world);
+  
+  svg.append("path")
+    .datum(topojson.feature(world, world.objects.countries))
+    .attr("d", d3.geo.path().projection(d3.geo.equirectangular()));
+
 });
