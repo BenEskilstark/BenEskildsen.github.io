b3c4f7c4fb6fcf08bcaefed1d0e46933d559c540
Benjamin Eskildsen <benjamin.eskildsen@yale.edu>
2014-09-25 14:17:01 -0400

    Let's see if panning and zooming works...

--- map/map.js
+++ map/map.js
@@ -5,14 +5,32 @@ var WORLDJSON = "world-50m.json";
 
 var width = 960;
 var height = 525;
+var scale0 = (width - 1) / 2 / Math.PI;
 
 var projection = d3.geo.equirectangular()
     .scale(150)
     .translate([width / 2, height / 2]);
 
+var zoom = d3.behavior.zoom()
+  .translate([width / 2, height / 2])
+  .scale(scale0)
+  .scaleExtent([scale0, 8 * scale0])
+  .on("zoom", zoomed);
+
+var path = d3.geo.path().projection(projection);
+
 var svg = d3.select("body").append("svg")
   .attr("width", width)
   .attr("height", height);
+
+svg.append("rect")
+  .attr("class", "overlay")
+  .attr("width", width)
+  .attr("height", height);
+
+svg
+  .call(zoom)
+  .call(zoom.event);
 
 d3.json(WORLDJSON, function(error, world) {
   if (error) return console.error(error);
@@ -21,7 +39,7 @@ d3.json(WORLDJSON, function(error, world) {
     .data(topojson.feature(world, world.objects.countries).features)
   .enter().append("path")
     .attr("class", function(d) { return "country_" + d.id; })
-    .attr("d", d3.geo.path().projection(projection))
+    .attr("d", path)
     .style({
       "visibility": 
       function(d) {
@@ -41,3 +59,12 @@ d3.json(WORLDJSON, function(error, world) {
     .attr("style", "visibility: hidden");
 
 });
+
+function zoomed() {
+  projection
+    .translate(zoom.translate())
+    .scale(zoom.scale());
+
+  g.selectAll("path")
+    .attr("d", path);
+}
