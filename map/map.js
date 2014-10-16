7ed471ae503da98bb140c96bfe4f3d18aa9ecf7f
Benjamin Eskildsen <benjamin.eskildsen@yale.edu>
2014-10-15 21:27:42 -0400

    Again...

--- map/map.js
+++ map/map.js
@@ -30,8 +30,8 @@ svg.append("defs").append("svg:clipPath")
   .attr("id", "clip")
   .append("svg:circle")
     .attr("id", "clip-circle")
-    .attr("cx", 0)
-    .attr("cy", 0)
+    .attr("cx", 50)
+    .attr("cy", 50)
     .attr("r", 40);
 
 var clipPath = svg.append("g")
