db977c97d58ecb3fbd3d2c601649acbc7662505a
Benjamin Eskildsen <benjamin.eskildsen@yale.edu>
2016-02-05 15:10:38 -0500

    added attempt buttons

--- compApp/js/comp.js
+++ compApp/js/comp.js
@@ -13,23 +13,50 @@ function seedClimbs(numClimbs) {
     return climbs;
 }
 
-function loadClimbs() {
-    var climbs = seedClimbs(10);
+function loadClimbs(climbs) {
     for (var i = 0, climb; climb = climbs[i]; i++) {
         var table = document.getElementById("table");
-        var li = document.createElement("li");
-        var points = document.createElement("div");
-        var color = document.createElement("div");
-        var attempts = document.createElement("div");
-
-        points.appendChild(document.createTextNode(climb.points));
-        color.appendChild(document.createTextNode(climb.color));
-        attempts.appendChild(document.createTextNode(climb.attempts));
-        li.appendChild(points);
-        li.appendChild(color);
-        li.appendChild(attempts);
-        table.appendChild(li);
+        table.appendChild(makeClimb(climbs, i));
     }
 }
 
-loadClimbs();
+function makeClimb(climbs, num) {
+    var climb = climbs[num];
+    var li = document.createElement("li");
+    li.id = num;
+    var points = document.createElement("div");
+    var color = document.createElement("div");
+    var attempts = document.createElement("div");
+    attempts.id = "attempts";
+    var button = document.createElement("button");
+
+    points.appendChild(document.createTextNode(climb.points));
+    color.appendChild(document.createTextNode(climb.color));
+    attempts.appendChild(document.createTextNode(climb.attempts));
+    button.appendChild(document.createTextNode("Add Attempt"));
+
+    button.onclick = buttonClick(climbs, num);
+
+    li.appendChild(points);
+    li.appendChild(color);
+    li.appendChild(attempts);
+    li.appendChild(button);
+
+    return li;
+}
+
+function clearClimbs(climbs) {
+    for (var i = 0; i < climbs.length; i++) {
+        document.getElementById("" + i).remove();
+    }
+}
+
+function buttonClick(climbs, num) {
+    return function() {
+        climbs[num].attempts += 1;
+        clearClimbs(climbs);
+        loadClimbs(climbs);
+    }
+}
+
+loadClimbs(seedClimbs(10));
